7412866fecfc210ec7cc466c15fcefec4cab15d9
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-20 09:49:21 -0300

    atribuição

---
+++ atribuição/atribuição.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// atribuição simples
+// int main() {
+//     const int a = 10; // Atribui 10 à variável a
+//     printf("a = %d\n", a);
+//     return 0;
+// }
+
+
+// atribuição de soma
+// int main() {
+//     int a = 5;
+//     a += 3; // Equivalente a a = a + 3
+//     printf("a += 3 -> %d\n", a);
+//     return 0;
+// }
+
+//atribuição de subtração
+
+
+// int main() {
+//     int a = 5;
+//     a -= 2; // Equivalente a a = a - 2
+//     printf("a -= 2 -> %d\n", a);
+//     return 0;
+// }
+
+
+//atribuição de multiplicação
+
+// int main() {
+//     int a = 5;
+//     a *= 2; // Equivalente a a = a * 2
+//     printf("a *= 2 -> %d\n", a);
+//     return 0;
+// }
+
+// atribuição de devisão
+
+
+// int main() {
+//     int a = 10;
+//     a /= 2; // Equivalente a a = a / 2
+//     printf("a /= 2 -> %d\n", a);
+//     return 0;
+// }
+
+
+// atribuição de modules
+//
+// int main() {
+//     int a = 10;
+//     a %= 3; // Equivalente a a = a % 3
+//     printf("a %%= 3 -> %d\n", a);
+//     return 0;
+// }
