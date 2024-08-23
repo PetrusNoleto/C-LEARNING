6bbad59db06d028e55ffe1f2ce065a57ecdabbb7
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-23 14:54:39 -0300

    teste

---
+++ time/time.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+#define LEN 150
+
+int main ()
+
+{
+
+    char buf[LEN];
+
+    time_t curtime;
+
+    struct tm *loc_time;
+    curtime = time (NULL);
+
+
+    loc_time = localtime (&curtime);
+
+
+
+    printf("%s", asctime (loc_time));
+
+    strftime (buf, LEN, "Hoje eh %A, %b %d.\n", loc_time);
+
+    fputs (buf, stdout);
+
+    strftime (buf, LEN, "A hora eh %I:%M:%S %p.\n", loc_time);
+
+    fputs (buf, stdout);
+
+    return 0;
+
+}
