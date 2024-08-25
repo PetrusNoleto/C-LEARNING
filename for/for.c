66bf25ddad1fc361bc3ef65cbfe3fa07235f1aa3
PETRUS NOLETO <88741218+PetrusNoleto@users.noreply.github.com>
2024-08-25 10:48:49 -0300

    create for.h

--- for/for.c
+++ for/for.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
-int main (){
+int ForExample() {
     const char* camisas[] = {"lacoste","nike","adidas"};
     const int tamanho = sizeof(camisas) / sizeof(camisas[0]);
     for (int i = 0; i < tamanho; i++) {
         printf("valor %s\n",camisas[i]);
     }
     return 0;
+}
+
+int main (){
+    ForExample();
 }
---
+++ for/for.h
@@ -0,0 +1,7 @@
+#ifndef for_h
+#define for_h
+
+extern int ForExample;
+int main();
+
+#endif // for_h
